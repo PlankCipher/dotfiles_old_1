d376f93726d71748824f98062d53ca6df5a5dbb3
PlankCipher <plankcipher1@protonmail.com>
2023-03-23 04:14:32 +0200

    feat: add misc dev stuff: nodejs, docker, dev dirs...

--- spice_it_up.sh
+++ spice_it_up.sh
@@ -34,8 +34,68 @@ EOF
   cd $SCRIPT_DIR
 }
 
+install_nodejs () {
+  cat <<EOF
+
+###################################################
+###                                             ###
+###              INSTALLING NODEJS              ###
+###                                             ###
+###################################################
+
+EOF
+
+  # NodeJS and YARN
+  sudo pacman -S nodejs
+
+  sudo corepack enable
+
+  yarn config set init-license GPL-3.0-only -g
+  yarn config set init-author-name PlankCipher -g
+
+  yarn global add eslint-cli create-react-app
+}
+
+install_misc_dev_stuff () {
+  cat <<EOF
+
+###################################################
+###                                             ###
+###          INSTALLING MISC DEV STUFF          ###
+###                                             ###
+###################################################
+
+EOF
+
+  sudo pacman -S man-pages
+
+  # docker
+  sudo pacman -S docker
+  sudo systemctl enable docker
+
+  # insomnia
+  yay -S insomnia-bin
+}
+
+copy_files_and_create_dirs () {
+  cat <<EOF
+
+###################################################
+###                                             ###
+###       COPYING FILES AND CREATING DIRS       ###
+###                                             ###
+###################################################
+
+EOF
+
+  mkdir -p $HOME/chamber_of_magic/junk/ $HOME/chamber_of_magic/test/
+}
+
 install_build_utils
 install_yay
+install_nodejs
+install_misc_dev_stuff
+copy_files_and_create_dirs
 
 cat << EOF
 __        __   _                               _                          _
