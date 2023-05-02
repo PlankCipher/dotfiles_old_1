6ed070bcfa270f3c538a566fa01e70f3e01b87d5
PlankCipher <plankcipher1@protonmail.com>
2023-05-02 19:48:08 +0300

    fix(nvim): use same background for modes in sections `b` & `c`

--- .config/nvim/lua/plankcipher/plugins/lualine.lua
+++ .config/nvim/lua/plankcipher/plugins/lualine.lua
@@ -1,6 +1,52 @@
+local colors = {
+  black = '#282828',
+  white = '#ebdbb2',
+  red = '#fb4934',
+  green = '#b8bb26',
+  blue = '#83a598',
+  yellow = '#fe8019',
+  gray = '#a89984',
+  darkgray = '#3c3836',
+  lightgray = '#504945',
+  inactivegray = '#7c6f64',
+}
+
+local gruvbox_dark_theme = {
+  normal = {
+    a = { bg = colors.gray, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.darkgray, fg = colors.gray },
+  },
+  insert = {
+    a = { bg = colors.blue, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.darkgray, fg = colors.gray },
+  },
+  visual = {
+    a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.darkgray, fg = colors.gray },
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.darkgray, fg = colors.gray },
+  },
+  command = {
+    a = { bg = colors.green, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.darkgray, fg = colors.gray },
+  },
+  inactive = {
+    a = { bg = colors.darkgray, fg = colors.gray, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.gray },
+    c = { bg = colors.darkgray, fg = colors.gray },
+  },
+}
+
 require('lualine').setup({
   options = {
-    theme = 'gruvbox_dark',
+    theme = gruvbox_dark_theme,
     component_separators = {left = '', right = ''},
     section_separators = {left = '', right = ''},
     always_divide_middle = false,
