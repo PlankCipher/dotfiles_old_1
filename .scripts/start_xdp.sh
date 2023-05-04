b8b7b9f0cdc2b63dd6f4342453e771bec95b7814
PlankCipher <plankcipher1@protonmail.com>
2023-05-05 01:11:06 +0300

    fix(hyprland): don't need xdg-desktop-portal-gtk

--- .scripts/start_xdp.sh
+++ .scripts/start_xdp.sh
@@ -4,13 +4,9 @@ sleep 1
 
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
-
-/usr/lib/xdg-desktop-portal-gtk &
 sleep 2
 
 /usr/lib/xdg-desktop-portal &
