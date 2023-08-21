210fa2c4dfdec81ff9bb6f589a3f5e2e993e0ae7
PlankCipher <plankcipher1@protonmail.com>
2023-08-22 00:22:24 +0300

    fix(rofi_song): remove extension from filename in rofi list

--- .scripts/rofi/rofi_song.sh
+++ .scripts/rofi/rofi_song.sh
@@ -2,7 +2,7 @@
 
 # Select a song to play through rofi
 
-NEXT_SONG="$(mpc listall | rofi -dmenu -p 'Play' -i)"
+NEXT_SONG="$(mpc listall | while read -r SONG_FILENAME; do echo ${SONG_FILENAME%.*}; done | rofi -dmenu -p 'Play' -i)"
 
 # Avoid playing next song if the previous
 # command exited with a non zero exit code
@@ -15,8 +15,9 @@ fi
 # Get next song's position in the playlist
 # (if exists) and play it
 POSITION=1
-mpc listall | while read -r SONG; do
-  if [[ "$SONG" == "$NEXT_SONG" ]]; then
+mpc listall | while read -r SONG_FILENAME; do
+  SONG_FILENAME="${SONG_FILENAME%.*}"
+  if [[ "$SONG_FILENAME" == "$NEXT_SONG" ]]; then
     mpc play $POSITION && sigdwmblocks 6
     break
   else
