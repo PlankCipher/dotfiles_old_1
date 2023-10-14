d24b3fdcdb6c32919253f564d3816364093719ae
PlankCipher <plankcipher1@protonmail.com>
2023-10-14 22:57:12 +0300

    fix(wezterm): reduce mouse scroll factor to `3`

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -174,4 +174,17 @@ config.keys = {
   }
 }
 
+config.mouse_bindings = {
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'NONE',
+    action = wezterm.action.ScrollByLine(-3),
+  },
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'NONE',
+    action = wezterm.action.ScrollByLine(3),
+  },
+}
+
 return config
