fe58098d1aae2e5acd209698b8631524998cac53
PlankCipher <plankcipher1@protonmail.com>
2023-10-18 14:18:10 +0300

    feat(nvim): remove float border of packer.nvim window

--- .config/nvim/lua/plankcipher/plugins/plugins.lua
+++ .config/nvim/lua/plankcipher/plugins/plugins.lua
@@ -61,7 +61,7 @@ end,
 config = {
   display = {
     open_fn = function()
-      return require('packer.util').float({border = 'single'})
+      return require('packer.util').float({border = 'none'})
     end
   }
 }})
